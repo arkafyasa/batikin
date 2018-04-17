68bd31ae8ac2a89de63e3e2e4dc943c308f34484
MuhammadSafreza <lord.ezza@gmail.com>
2018-04-17 23:38:02 +0700

    ganti password

--- app/src/main/java/com/batikin/vocomfest/batikin/ActivityPengaturan.java
+++ app/src/main/java/com/batikin/vocomfest/batikin/ActivityPengaturan.java
@@ -1,14 +1,73 @@
 package com.batikin.vocomfest.batikin;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class ActivityPengaturan extends AppCompatActivity {
+import com.batikin.vocomfest.batikin.utils.CDM;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ActivityPengaturan extends AppCompatActivity implements View.OnClickListener, CDM {
+
+    private EditText lama;
+    private EditText baru;
+    private Button ok;
+    private FirebaseUser user;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pengaturan_layout);
 
+        lama = findViewById(R.id.txt_pengaturan_pass_lama);
+        baru = findViewById(R.id.txt_pengaturan_pass_baru);
+        ok = findViewById(R.id.btn_account_photo);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        ok.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        gantiPassword(lama.getText().toString(), baru.getText().toString());
+    }
+
+    public void gantiPassword(String lama, final String baru) {
+        AuthCredential credential = EmailAuthProvider
+                .getCredential(user.getEmail(), lama);
+
+// Prompt the user to re-provide their sign-in credentials
+        user.reauthenticate(credential)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            user.updatePassword(baru).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    if (task.isSuccessful()) {
+                                        Log.d(TAG, "Password updated");
+                                        Toast.makeText(getApplicationContext(), "password berhasil d diubah", Toast.LENGTH_LONG).show();
+                                    } else {
+                                        Log.d(TAG, "Error password not updated");
+                                    }
+                                }
+                            });
+                        } else {
+                            Log.d(TAG, "Error auth failed");
+                        }
+                    }
+                });
     }
 }
--- app/src/main/java/com/batikin/vocomfest/batikin/ActivityRiwayat.java
+++ app/src/main/java/com/batikin/vocomfest/batikin/ActivityRiwayat.java
@@ -3,11 +3,81 @@ package com.batikin.vocomfest.batikin;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+import com.batikin.vocomfest.batikin.model.PemesananModel;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ActivityRiwayat extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private DatabaseReference mDatabaseRiwayat;
+    private FirebaseUser mCurrentUser;
+
+
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.riwayat_layout);
+
+
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_riwayat);
+        mRecyclerView.setHasFixedSize(true);
+        mDatabaseRiwayat = FirebaseDatabase.getInstance().getReference().child("Pemesanan").child(mCurrentUser.getUid());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<PemesananModel, PemesananViewHolder>
+                firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<PemesananModel, PemesananViewHolder>
+                (PemesananModel.class,R.layout.riwayat_item,PemesananViewHolder.class,mDatabaseRiwayat) {
+            @Override
+            protected void populateViewHolder(PemesananViewHolder viewHolder, PemesananModel model, int position) {
+                viewHolder.setHarga(model.getHarga());
+                viewHolder.setMotif(model.getMotif());
+                viewHolder.setDesc(model.getModel(),model.getKategori(),model.getUkuran());
+            }
+        };
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(firebaseRecyclerAdapter);
+    }
+
+    public static class PemesananViewHolder extends RecyclerView.ViewHolder {
+        View mView;
+        private TextView motivText;
+        private TextView hargatext;
+        private TextView desc;
+
+        public PemesananViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+            motivText = mView.findViewById(R.id.motif_text);
+            hargatext = mView.findViewById(R.id.harga_text);
+            desc = mView.findViewById(R.id.model_ukuran_pengiriman_text);
+        }
+
+        public void setMotif(String text){
+            motivText.setText(text);
+        }
+
+        public void setHarga(String text){
+            hargatext.setText("Rp. "+text);
+        }
+
+        public void setDesc(String model, String kategori, String ukuran){
+            desc.setText("Model :"+model +"\n"
+                        +"Kategori : "+kategori+"\n"
+                        +"ukuran : "+ukuran);
+        }
     }
 }
